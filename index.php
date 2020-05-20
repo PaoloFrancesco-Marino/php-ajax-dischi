88026d57c3844b4d660adf37ddd44053c0e381b6
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-05-20 13:46:27 +0200

    add initial file

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Ajax - Dischi</title>
+
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="dist/css/main.css">
+</head>
+
+<body>
+
+
+        <!-- Main JS -->
+        <script src="dist/js/main.js"></script>
+    
+</body>
+</html>
